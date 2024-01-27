3a7b41316a304e5f937f9a3fb57c8972b7337bb4
Mahmudul Hasan <dev.mahmudulhasan@gmail.com>
2024-01-27 16:56:01 +0200

    All markup setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import { useState } from "react";
 import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
+import TaskBoard from "./components/TaskBoard";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1 className="text-red-300">Test</h1>
-    </>
+    <div className="bg-[#191D26] font-[Inter] text-white">
+      <Header />
+      <HeroSection />
+      <TaskBoard />
+      <Footer />
+    </div>
   );
 }
 
